a21fb0a785acfd229dd40308f389697460a92955
卜错 <baiming.org@gmail.com>
2015-02-22 16:17:23 +0800

    finish coding

--- archiveDemo/ViewController.m
+++ archiveDemo/ViewController.m
@@ -16,7 +16,59 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    /**
+        归档对象
+     */
+//    NSArray *data = @[@"I",@"am",@"a",@"student"];
+//    NSString *home = NSHomeDirectory();
+//    NSString *path = [home stringByAppendingPathComponent:@"test.arc"];
+//    BOOL result = [NSKeyedArchiver archiveRootObject:data toFile:path];
+//    if (result) {
+//        NSLog(@"%@",home);
+//
+//    }
+//
+    /**
+     *  解归档
+     *
+     *  @return <#return value description#>
+     */
+//    NSString *home = NSHomeDirectory();
+//    NSString *path = [home stringByAppendingPathComponent:@"test.arc"];
+//    NSArray *data = [NSKeyedUnarchiver unarchiveObjectWithFile:path];
+//    NSLog(@"%@",data);
+    
+    
+    /**
+     *  归档
+     *
+     *  @return <#return value description#>
+     */
+    NSString *home = NSHomeDirectory();
+    NSLog(@"%@",home);
+    NSString *path = [home stringByAppendingPathComponent:@"custome.arc"];
+    NSMutableData *data = [NSMutableData data];
+    NSKeyedArchiver *archive = [[NSKeyedArchiver alloc]
+                                initForWritingWithMutableData:data];
+    
+    [archive encodeFloat:50.0 forKey:@"weight"];
+    [archive encodeObject:@"jack" forKey:@"name"];
+    [archive finishEncoding];
+    BOOL success = [data writeToFile:path atomically:YES];
+    if (success) {
+        //success
+    }
+    
+    NSData *content = [NSData dataWithContentsOfFile:path];
+    NSKeyedUnarchiver *unarchive = [[NSKeyedUnarchiver alloc]
+                                    initForReadingWithData:content];
+    
+    float weight = [unarchive decodeFloatForKey:@"weight"];
+    NSString *name = [unarchive decodeObjectForKey:@"name"];
+                      
+                      
+    
 }
 
 - (void)didReceiveMemoryWarning {
