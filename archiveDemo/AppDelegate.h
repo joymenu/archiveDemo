bcbb54a4400eccde48b0ef5912bd5547a6b8c5e8
卜相刚 <buxianggang697@pingan.com.cn>
2015-02-22 15:36:02 +0800

    Initial Commit

---
+++ archiveDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  archiveDemo
+//
+//  Created by bbu on 2/22/15.
+//  Copyright (c) 2015 bbu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
